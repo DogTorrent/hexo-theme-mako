f726327a39e569064229dfc3b9d115ee8cc97b60
DotTorrent <pointtorrent@outlook.com>
2021-01-17 00:43:14 +0800

    增加了对pjax的支持，移除了valine以外的评论系统支持，修复了valine一些kv键值不生效的bug
    删除_partial\目录下的 _comment\目录，并在原位增加_after-footer\目录。
    其实也就是将评论系统的script放到#nexmoe-content外（after-footer），同时保持评论系统的块仍在#nexmoe-content内。
    （ps #nexmoe-content是ajax的更新区域）

--- source/js/app.js
+++ source/js/app.js
@@ -20,20 +20,52 @@ const getRealPath = (pathname, desc = false) => {
   }
   return "/";
 };
-let links = document.querySelectorAll(".nexmoe-list-item");
-let rootRealPath = getRealPath(window.location.pathname, true);
-for (let link of links) {
-  let linkPath = link.getAttribute("href");
-  if (linkPath && getRealPath(linkPath, true) === rootRealPath) {
-    link.className = "active nexmoe-list-item mdui-list-item mdui-ripple";
-  }
-}
 
-$("table")
-  .has("img")
-  .addClass("nexmoe-album");
+function setListItemClass() {
+  let links = document.querySelectorAll(".nexmoe-list-item");
+  let rootRealPath = getRealPath(window.location.pathname, true);
+  for (let link of links) {
+    let linkPath = link.getAttribute("href");
+    if (linkPath && getRealPath(linkPath, true) === rootRealPath) {
+      link.className = "active nexmoe-list-item mdui-list-item mdui-ripple";
+    } else {
+      link.className = "nexmoe-list-item mdui-list-item mdui-ripple";
+    }
+  }
+};
+
+function addNexmoeAlbumClass() {
+  $("table")
+    .has("img")
+    .addClass("nexmoe-album");
+}
 
 function search() {
   window.open($("#search_form").attr("action_e") + " " + $("#search_value").val());
   return false;
 }
+
+setListItemClass();
+
+document.addEventListener('pjax:complete', function(){
+	var scriptsToBeReload=document.querySelectorAll("script.pjax-reload, .pjax-reload script");
+	for (var element of scriptsToBeReload) {
+		var id = element.id || "";
+		var src = element.src || "";
+		var code = element.text || element.textContent || element.innerHTML || "";
+		var parent = element.parentNode;
+		var script = document.createElement("script");
+		var className = element.className || "";
+		parent.removeChild(element);
+		if (id !== "") { script.id = element.id; }
+		if (className !== "") { script.className=className; }
+		if (src !== "") { script.src = src.replace(/\?v=[0-9]*/i,"") + "?v=" + Number(new Date()); }
+		if (code !== "") { script.appendChild(document.createTextNode(code)); }
+		parent.appendChild(script);
+	}
+	setListItemClass();
+	addNexmoeAlbumClass();
+	document.querySelectorAll('pre code').forEach((block) => {
+		hljs.highlightBlock(block);
+	});
+});
